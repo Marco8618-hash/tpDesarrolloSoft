1c649592f4aa7252de18a7997a0a8c9eb2fdd19d
Marco Tessore <marcoeltrebol@gmail.com>
2026-08-06 11:20:00 -0300

    alta de configuracion de express y app.ts

--- Techrepair/app.ts
+++ Techrepair/app.ts
@@ -1,3 +1,17 @@
 console.log("aca arranca el server")
 
 
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('¡Servidor Express con TypeScript en marcha!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
