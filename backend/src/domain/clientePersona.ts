6bc55b1f52e2a09c9ba32017b0301943309e37d4
Marco Tessore <marcoeltrebol@gmail.com>
2026-08-13 16:02:27 -0300

    agregados decoradores de ORM a clientePersona

--- backend/src/domain/clientePersona.ts
+++ backend/src/domain/clientePersona.ts
@@ -1,3 +1,7 @@
+// CLASE CLIENTE PERSONA
+
+/*
+
 import { Cliente } from './cliente';
 
 export class ClientePersona extends Cliente {
@@ -8,5 +12,28 @@ export class ClientePersona extends Cliente {
     super(id, cuit);
     this.nombre = nombre;
     this.apellido = apellido;
+  }
+}
+
+*/
+
+import { Cliente } from "./cliente";
+import { Entity, Property } from "@mikro-orm/decorators/legacy";
+
+@Entity({
+  tableName: 'clientes_persona',
+})
+
+export class ClientePersona extends Cliente {
+  @Property({ fieldName: 'nombre', length: 255 })
+  nombre!: string;
+
+  @Property({ fieldName: 'apellido', length: 255 })
+  apellido!: string;
+
+  constructor(id?: number, cuit?: string, nombre?: string, apellido?: string) {
+    super(id, cuit);
+    if (nombre !== undefined) this.nombre = nombre;
+    if (apellido !== undefined) this.apellido = apellido;
   }
 }
